f30070027c3b30f5887f6a57036b0ef4e4857843
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-14 18:06:05 -0300

    Adicionando WhateverContext

--- 4 - Infra/4.1 - Data/Microservice.Whatevers.Data/Context/WhateverContext.cs
+++ 4 - Infra/4.1 - Data/Microservice.Whatevers.Data/Context/WhateverContext.cs
@@ -1,10 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using Microservice.Whatevers.Domain.Entities;
+using Microservice.Whatevers.Data.Config;
 
 namespace Microservice.Whatevers.Data.Context
 {
     public class WhateverContext : DbContext
     {
-        public DbSet<Whatever> MyProperty { get; set; }
+        public DbSet<Whatever> Whatevers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseInMemoryDatabase(databaseName: "WhateverDb");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Whatever>(new WhateverConfig().Configure);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
