c5ade124955fbd08400c82e9b7c01fb7955048ce
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-15 17:11:56 -0300

    Ajuste na IRepository

--- 4 - Infra/4.1 - Data/Microservice.Whatevers.Data/Repositories/IRepository.cs
+++ 4 - Infra/4.1 - Data/Microservice.Whatevers.Data/Repositories/IRepository.cs
@@ -1,15 +1,15 @@
+using System;
 using Microservice.Whatevers.Domain.Entities;
 using System.Collections.Generic;
 
-namespace Microservice.Whatevers.Data.Repository
+namespace Microservice.Whatevers.Data.Repositories
 {
-    public interface IRepository<T> 
-    where T : BaseEntity
+    public interface IRepository<T> where T : BaseEntity
     {
         void Insert(T entity);
         void Update(T entity);
-        void Delete(int id);
-        T Select(int id);
+        void Delete(Guid id);
+        T Select(Guid id);
         IList<T> SelectAll();
     }
 }
