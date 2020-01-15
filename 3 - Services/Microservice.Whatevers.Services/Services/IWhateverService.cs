3c613dd3f132788e8078ef0831556d50e4176863
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-15 17:11:01 -0300

    Criando IWhateverService

---
+++ 3 - Services/Microservice.Whatevers.Services/Services/IWhateverService.cs
@@ -0,0 +1,9 @@
+using Microservice.Whatevers.Domain.Entities;
+
+namespace Microservice.Whatevers.Services.Services
+{
+    public interface IWhateverService : IService<Whatever>
+    {
+         
+    }
+}
