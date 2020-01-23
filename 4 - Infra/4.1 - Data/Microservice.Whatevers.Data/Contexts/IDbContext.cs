adc17cd21686fe07509d85f8cde7fb064e68a34b
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-23 17:43:39 -0300

    Adicionando recurso assíncrono ao DbContext

--- 4 - Infra/4.1 - Data/Microservice.Whatevers.Data/Contexts/IDbContext.cs
+++ 4 - Infra/4.1 - Data/Microservice.Whatevers.Data/Contexts/IDbContext.cs
@@ -1,12 +1,15 @@
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Microservice.Whatevers.Data.Contexts
 {
-    public interface IDbContext 
+    public interface IDbContext
     {
-          DbSet<TEntity> Set<TEntity>() where TEntity : class;
-          EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
-          int SaveChanges();
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+        int SaveChanges();
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
     }
 }
