c708cabe1d3161d047881692902bc659ef05818a
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-23 17:45:06 -0300

    Adicionando recurso assincrono ao repositorio

--- 4 - Infra/4.1 - Data/Microservice.Whatevers.Data/Repositories/IRepository.cs
+++ 4 - Infra/4.1 - Data/Microservice.Whatevers.Data/Repositories/IRepository.cs
@@ -1,15 +1,29 @@
+using System.Threading.Tasks;
 using System;
 using Microservice.Whatevers.Domain.Entities;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Microservice.Whatevers.Data.Repositories
 {
     public interface IRepository<T> where T : BaseEntity
     {
         void Insert(T entity);
+        Task InsertAsync(T entity, CancellationToken cancellationToken);
+
         void Update(T entity);
+        Task UpdateAsync(T entity, CancellationToken cancelletionToken);
+
         void Delete(Guid id);
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken);
+
         T Select(Guid id);
+        Task<T> SelectAsync(Guid id, CancellationToken cancellationToken);
+
         IList<T> SelectAll();
+        Task<IList<T>> SelectAllAsync(CancellationToken cancellationToken);
+        
+        bool Exists(Guid id);
+        Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken);
     }
 }
