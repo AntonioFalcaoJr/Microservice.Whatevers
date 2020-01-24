fe007b1245862eab76f87b4e3b35d0621626dcf7
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-24 18:08:03 -0300

    Melhoria de validações e uso de AsNotracking() para melhor performance e evitar o trackeamento desnecessário

--- 4 - Infra/4.1 - Data/Microservice.Whatevers.Data/Repositories/BaseRepository.cs
+++ 4 - Infra/4.1 - Data/Microservice.Whatevers.Data/Repositories/BaseRepository.cs
@@ -12,60 +12,72 @@ namespace Microservice.Whatevers.Data.Repositories
     public abstract class BaseRepository<T> : IRepository<T> where T : BaseEntity
     {
         private readonly IWhateverContext _context;
+        private readonly DbSet<T> _dbSet;
 
-        public BaseRepository(IWhateverContext whateverContext)
+        protected BaseRepository(IWhateverContext whateverContext)
         {
             _context = whateverContext;
+            _dbSet = whateverContext.Set<T>();
         }
 
         public void Delete(Guid id)
         {
-            _context.Set<T>().Remove(Select(id));
+            _dbSet.Remove(SelectById(id));
             _context.SaveChanges();
         }
 
         public async Task DeleteAsync(Guid id, CancellationToken cancellationToken)
         {
-            _context.Set<T>().Remove(Select(id));
+            _dbSet.Remove(await SelectByIdAsync(id, cancellationToken));
             await _context.SaveChangesAsync(true, cancellationToken);
         }
 
-        public bool Exists(Guid id) => Select(id) is null ? default : true;
+        public bool Exists(Guid id) => 
+            _dbSet.AsNoTracking().Any(x => x.Id == id);
 
         public async Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken) => 
-         (await SelectAsync(id, cancellationToken)) is null ? default : true;
+            await _dbSet.AsNoTracking().AnyAsync(x => x.Id == id, cancellationToken);
 
         public void Insert(T entity)
         {
-            _context.Set<T>().Add(entity);
+            if (Exists(entity.Id)) return;
+
+            _dbSet.Add(entity);
             _context.SaveChanges();
         }
 
         public async Task InsertAsync(T entity, CancellationToken cancellationToken)
         {
-            await _context.Set<T>().AddAsync(entity, cancellationToken);
+            if (await ExistsAsync(entity.Id, cancellationToken)) return;
+            
+            await _dbSet.AddAsync(entity, cancellationToken);
             await _context.SaveChangesAsync(true, cancellationToken);
         }
 
-        public T Select(Guid id) => _context.Set<T>().Find(id);
+        public T SelectById(Guid id) => 
+            _dbSet.AsNoTracking().FirstOrDefault(x => x.Id == id);
 
-        public async Task<T> SelectAsync(Guid id, CancellationToken cancellationToken) =>
-            await _context.Set<T>().FindAsync(id, cancellationToken);
+        public async Task<T> SelectByIdAsync(Guid id, CancellationToken cancellationToken) => 
+            await _dbSet.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
 
-        public IList<T> SelectAll() => _context.Set<T>().ToList();
+        public IList<T> SelectAll() => 
+            _dbSet.AsNoTracking().ToList();
 
-        public async Task<IList<T>> SelectAllAsync(CancellationToken cancellationToken) =>
-            await _context.Set<T>().ToListAsync();
-
+        public async Task<IList<T>> SelectAllAsync(CancellationToken cancellationToken) => 
+            await _dbSet.AsNoTracking().ToListAsync(cancellationToken);
 
         public void Update(T entity)
         {
+            if (!Exists(entity.Id)) return;
+
             _context.Entry(entity).State = EntityState.Modified;
             _context.SaveChanges();
         }
 
         public async Task UpdateAsync(T entity, CancellationToken cancellationToken)
         {
+            if (!await ExistsAsync(entity.Id, cancellationToken)) return;
+            
             _context.Entry(entity).State = EntityState.Modified;
             await _context.SaveChangesAsync(true, cancellationToken);
         }
