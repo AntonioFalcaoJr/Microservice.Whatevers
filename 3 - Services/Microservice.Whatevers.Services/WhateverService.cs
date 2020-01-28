f83002df30b42c0d15b5d93f052fdca127937819
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-28 17:02:21 -0300

    Injetando AutoMapper

--- 3 - Services/Microservice.Whatevers.Services/WhateverService.cs
+++ 3 - Services/Microservice.Whatevers.Services/WhateverService.cs
@@ -1,12 +1,13 @@
+using AutoMapper;
 using Microservice.Whatevers.Data.Repositories;
 using Microservice.Whatevers.Domain.Entities;
 using Microservice.Whatevers.Services.Models;
 
 namespace Microservice.Whatevers.Services
 {
-    public class WhateverService : BaseService<Whatever, WhateverModel>
+    public class WhateverService : BaseService<Whatever, WhateverModel>, IWhateverService
     {
-        public WhateverService(IWhateverRepository whateverRepository) 
-        : base(whateverRepository) { }
+        public WhateverService(IWhateverRepository whateverRepository, IMapper mapper) 
+        : base(whateverRepository, mapper) { }
     }
 }
