d06c95c396f1e439f357a8621018e2db9ce6fe56
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-28 16:56:16 -0300

    Adicionando ModelBase

--- 3 - Services/Microservice.Whatevers.Services/Models/ThingModel.cs
+++ 3 - Services/Microservice.Whatevers.Services/Models/ThingModel.cs
@@ -2,9 +2,8 @@ using System;
 
 namespace Microservice.Whatevers.Services.Models
 {
-    public class ThingModel
+    public class ThingModel : BaseModel
     {
-        public Guid? Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public double Value { get; set; }
