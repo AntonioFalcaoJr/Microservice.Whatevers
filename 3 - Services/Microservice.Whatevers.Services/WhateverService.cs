c924fa2bc4adab07e54a210bd06bffd686b92249
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-27 22:16:51 -0300

    Remoção de interface desnecessária

--- 3 - Services/Microservice.Whatevers.Services/WhateverService.cs
+++ 3 - Services/Microservice.Whatevers.Services/WhateverService.cs
@@ -1,9 +1,10 @@
 using Microservice.Whatevers.Data.Repositories;
 using Microservice.Whatevers.Domain.Entities;
+using Microservice.Whatevers.Services.Models;
 
 namespace Microservice.Whatevers.Services
 {
-    public class WhateverService : BaseService<Whatever>, IWhateverService
+    public class WhateverService : BaseService<Whatever, WhateverModel>
     {
         public WhateverService(IWhateverRepository whateverRepository) 
         : base(whateverRepository) { }
