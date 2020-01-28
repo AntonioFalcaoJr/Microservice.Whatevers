9399c87acbff376f40dea5e163a35a7011700f57
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-28 16:49:28 -0300

    Incluindo AutoMapper ao Services e melhorando abstração da camada

--- 3 - Services/Microservice.Whatevers.Services/BaseService.cs
+++ 3 - Services/Microservice.Whatevers.Services/BaseService.cs
@@ -1,21 +1,28 @@
-using System;
-using System.Collections.Generic;
-using Microservice.Whatevers.Domain.Entities;
 using Microservice.Whatevers.Data.Repositories;
+using Microservice.Whatevers.Services.Models;
+using Microservice.Whatevers.Domain.Entities;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Linq;
+using AutoMapper;
+using System;
 
 namespace Microservice.Whatevers.Services
 {
-    public abstract class BaseService<TEntity, TModel> : IService<TEntity> 
+    public abstract class BaseService<TEntity, TModel> : IService<TEntity, TModel> 
         where TEntity : BaseEntity 
-        where TModel: class
+        where TModel: BaseModel
     {
         private readonly IRepository<TEntity> _repository;
+        private readonly IMapper _mapper;
 
-        protected BaseService(IRepository<TEntity> repository)
+        protected BaseService(IRepository<TEntity> repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         public void Delete(Guid id)
@@ -34,14 +41,16 @@ namespace Microservice.Whatevers.Services
             await _repository.DeleteAsync(id, cancellationToken);
         }
 
-        public TEntity Edit(TEntity entity)
+        public TEntity Edit(TModel model)
         {
+            var entity = _mapper.Map<TEntity>(model);
             _repository.Update(entity);
             return entity;
         }
 
-        public async Task<TEntity> EditAsync(TEntity entity, CancellationToken cancellationToken)
+        public async Task<TEntity> EditAsync(TModel model, CancellationToken cancellationToken)
         {
+            var entity = _mapper.Map<TEntity>(model);
             await _repository.UpdateAsync(entity, cancellationToken);
             return entity;
         }
@@ -51,29 +60,28 @@ namespace Microservice.Whatevers.Services
         public async Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken) => 
             await _repository.ExistsAsync(id, cancellationToken);
 
-        public IEnumerable<TEntity> GetAll() => _repository.SelectAll();
+        public IList<TModel> GetAll() => 
+            _repository.SelectAll().ProjectTo<TModel>(_mapper.ConfigurationProvider).ToList();
 
-        public async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken) => 
-            await _repository.SelectAllAsync(cancellationToken);
+        public async Task<IList<TModel>> GetAllAsync(CancellationToken cancellationToken) =>
+            await _repository.SelectAll().ProjectTo<TModel>(_mapper.ConfigurationProvider)
+               .ToListAsync(cancellationToken);
 
-        public TEntity GetById(Guid id) => 
-            id == Guid.Empty
-                ? throw new ArgumentException("Id inválido")
-                : _repository.SelectById(id);
+        public TModel GetById(Guid id) => _mapper.Map<TModel>(_repository.SelectById(id));
 
-        public async Task<TEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken) => 
-            id == Guid.Empty
-                ? throw new ArgumentException("Id inválido")
-                : await _repository.SelectByIdAsync(id, cancellationToken);
+        public async Task<TModel> GetByIdAsync(Guid id, CancellationToken cancellationToken) =>
+            _mapper.Map<TModel>(await _repository.SelectByIdAsync(id, cancellationToken));
 
-        public TEntity Save(TEntity entity)
+        public TEntity Save(TModel model)
         {
+            var entity = _mapper.Map<TEntity>(model);
             _repository.Insert(entity);
             return entity;
         }
 
-        public async Task<TEntity> SaveAsync(TEntity entity, CancellationToken cancellationToken)
+        public async Task<TEntity> SaveAsync(TModel model, CancellationToken cancellationToken)
         {
+            var entity = _mapper.Map<TEntity>(model);
             await _repository.InsertAsync(entity, cancellationToken);
             return entity;
         }
