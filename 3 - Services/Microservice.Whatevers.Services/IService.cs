f25d83345aba07ef367d3e0a681be44f9010c372
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-28 16:53:49 -0300

    Utilizando modelo no serviço

--- 3 - Services/Microservice.Whatevers.Services/IService.cs
+++ 3 - Services/Microservice.Whatevers.Services/IService.cs
@@ -3,25 +3,28 @@ using System;
 using System.Collections.Generic;
 using Microservice.Whatevers.Domain.Entities;
 using System.Threading.Tasks;
+using Microservice.Whatevers.Services.Models;
 
 namespace Microservice.Whatevers.Services
 {
-    public interface IService<TEntity> where TEntity : BaseEntity
+    public interface IService<TEntity, TModel> 
+        where TEntity : BaseEntity 
+        where TModel : BaseModel
     {
-        TEntity Save(TEntity entity);
-        Task<TEntity> SaveAsync(TEntity entity, CancellationToken cancellationToken);
+        TEntity Save(TModel model);
+        Task<TEntity> SaveAsync(TModel model, CancellationToken cancellationToken);
 
-        TEntity Edit(TEntity entity);
-        Task<TEntity> EditAsync(TEntity entity, CancellationToken cancellationToken);
+        TEntity Edit(TModel model);
+        Task<TEntity> EditAsync(TModel model, CancellationToken cancellationToken);
 
         void Delete(Guid id);
         Task DeleteAsync(Guid id, CancellationToken cancellationToken);
 
-        TEntity GetById(Guid id);
-        Task<TEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        TModel GetById(Guid id);
+        Task<TModel> GetByIdAsync(Guid id, CancellationToken cancellationToken);
 
-        IEnumerable<TEntity> GetAll();
-        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken);
+        IList<TModel> GetAll();
+        Task<IList<TModel>> GetAllAsync(CancellationToken cancellationToken);
 
         bool Exists(Guid id);
         Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken);
