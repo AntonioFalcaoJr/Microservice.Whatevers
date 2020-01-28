484c0b3ec4690e431f0982bba7055d2f4ceb6c9e
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-27 22:15:45 -0300

    Alterando controlller para utilizar FluentValidation via framework

--- 1 - Application/Microservice.Whatevers.Api/Controllers/WhateverController.cs
+++ 1 - Application/Microservice.Whatevers.Api/Controllers/WhateverController.cs
@@ -1,4 +1,3 @@
-using Microservice.Whatevers.Services.Validators;
 using Microservice.Whatevers.Domain.Entities;
 using Microservice.Whatevers.Services;
 using System.Collections.Generic;
@@ -6,36 +5,33 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using System.Threading;
 using System;
+using FluentValidation;
 
 namespace Microservice.Whatevers.Api.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
+    [ApiController,Route("[controller]")]
     public class WhateverController : ControllerBase
     {
         private readonly IWhateverService _whateverService;
-        private readonly IWhateverValidator _validator;
 
-        public WhateverController(IWhateverService whateverService, IWhateverValidator validator)
+        public WhateverController(IWhateverService whateverService, IValidator<Whatever> validator)
         {
             _whateverService = whateverService;
-            _validator = validator;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Whatever>>> GetAllAsync(CancellationToken cancellationToken)
         {
             var whatevers = await _whateverService.GetAllAsync(cancellationToken);
-            if (whatevers is {Count: 0}) return NotFound();
+            if (whatevers is {Count: 0}) return NoContent();
 
             return Ok(whatevers);
         }
 
-        [HttpGet("{id}")]
-        [ActionName("GetByIdAsync")]
+        [HttpGet("{id}"),ActionName("GetByIdAsync")]
         public async Task<IActionResult> GetByIdAsync(Guid id, CancellationToken cancellationToken)
         {
-            if (id == Guid.Empty) return BadRequest();
+            if (Guid.Empty == id) return BadRequest();
 
             var whatever = await _whateverService.GetByIdAsync(id, cancellationToken);
             if (whatever is null) return NotFound();
@@ -46,11 +42,6 @@ namespace Microservice.Whatevers.Api.Controllers
         [HttpPost]
         public async Task<IActionResult> PostAsync([FromBody] Whatever whatever, CancellationToken cancellationToken)
         {
-            if (whatever is null) return BadRequest();
-            
-            var validationResult = await _validator.ValidateAsync(whatever,cancellationToken);
-            if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
-
             await _whateverService.SaveAsync(whatever, cancellationToken);
             return CreatedAtAction(nameof(GetByIdAsync), new { id = whatever.Id, cancellationToken }, whatever);
         }
@@ -58,11 +49,7 @@ namespace Microservice.Whatevers.Api.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> PutAsync(Guid id, [FromBody] Whatever whatever, CancellationToken cancellationToken)
         {
-            if (Guid.Empty == id) return BadRequest();
-            if (whatever?.Id != id) return BadRequest();
-            
-            var validationResult = await _validator.ValidateAsync(whatever, cancellationToken);
-            if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
+            if (Guid.Empty == id || whatever?.Id != id) return BadRequest();
             
             await _whateverService.EditAsync(whatever, cancellationToken);
             return Ok(whatever);
@@ -71,9 +58,9 @@ namespace Microservice.Whatevers.Api.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteAsync(Guid id, CancellationToken cancellationToken)
         {
-            if (id == Guid.Empty) return BadRequest();
+            if (Guid.Empty == id) return BadRequest();
             if (!await _whateverService.ExistsAsync(id, cancellationToken)) return NotFound();
-
+            
             await _whateverService.DeleteAsync(id, cancellationToken);
             return Accepted();
         }
