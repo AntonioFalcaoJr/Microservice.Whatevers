9c59b3127e21e8a28a612f342ba899bee37b4dad
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-30 22:09:33 -0300

    Code clean up

--- 1 - Application/Microservice.Whatevers.Api/Startup.cs
+++ 1 - Application/Microservice.Whatevers.Api/Startup.cs
@@ -2,23 +2,37 @@ using AutoMapper;
 using FluentValidation.AspNetCore;
 using Microservice.Whatevers.Data;
 using Microservice.Whatevers.Services;
+using Microservice.Whatevers.Services.Mapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microservice.Whatevers.Services.Mapper;
 
 namespace Microservice.Whatevers.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -31,26 +45,6 @@ namespace Microservice.Whatevers.Api
 
             IocServices.Register(services);
             IoCData.Register(services);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
         }
     }
 }
