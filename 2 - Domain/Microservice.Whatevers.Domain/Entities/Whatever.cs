f66b86a50d154dedabd9e89ec4ab8e37820bd546
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-30 22:09:46 -0300

    Code clean up

--- 2 - Domain/Microservice.Whatevers.Domain/Entities/Whatever.cs
+++ 2 - Domain/Microservice.Whatevers.Domain/Entities/Whatever.cs
@@ -6,8 +6,8 @@ namespace Microservice.Whatevers.Domain.Entities
     public class Whatever : BaseEntity
     {
         public string Name { get; set; }
-        public string  Type { get; set; }
-        public DateTime Time { get; set; }
         public IList<Thing> Things { get; set; }
+        public DateTime Time { get; set; }
+        public string Type { get; set; }
     }
 }
