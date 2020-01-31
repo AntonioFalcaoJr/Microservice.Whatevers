1c0fa047a0b17c6c1b2fae9f91bcb9a80065ed3d
antonio.falcao <antonio.falcao@softplan.com.br>
2020-01-30 22:10:16 -0300

    Code clean up

--- 3 - Services/Microservice.Whatevers.Services/Models/WhateverModel.cs
+++ 3 - Services/Microservice.Whatevers.Services/Models/WhateverModel.cs
@@ -6,8 +6,8 @@ namespace Microservice.Whatevers.Services.Models
     public class WhateverModel : BaseModel
     {
         public string Name { get; set; }
-        public string  Type { get; set; }
-        public DateTime Time { get; set; }
         public IList<ThingModel> Things { get; set; }
+        public DateTime Time { get; set; }
+        public string Type { get; set; }
     }
 }
