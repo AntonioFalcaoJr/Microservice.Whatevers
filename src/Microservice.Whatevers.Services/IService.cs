97ddb71556fcd636d3c68c4c2aac1a66ed08748d
antoniofalcao <antonio.falcao@softplan.com.br>
2020-02-03 19:13:18 -0300

    Code CleanUp

--- src/Microservice.Whatevers.Services/IService.cs
+++ src/Microservice.Whatevers.Services/IService.cs
@@ -1,7 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using Microservice.Whatevers.Domain.Entities;
 using Microservice.Whatevers.Services.Models;
 
