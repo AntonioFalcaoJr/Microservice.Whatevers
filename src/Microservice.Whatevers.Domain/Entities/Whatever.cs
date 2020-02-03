2f50550dfb06465045f30ea78d9eea4f83130836
antoniofalcao <antonio.falcao@softplan.com.br>
2020-02-03 19:06:55 -0300

    Adicionando a diretiva Virtual para sobrescrita do EFCore

--- src/Microservice.Whatevers.Domain/Entities/Whatever.cs
+++ src/Microservice.Whatevers.Domain/Entities/Whatever.cs
@@ -6,7 +6,7 @@ namespace Microservice.Whatevers.Domain.Entities
     public class Whatever : BaseEntity
     {
         public string Name { get; set; }
-        public IList<Thing> Things { get; set; }
+        public virtual ICollection<Thing> Things { get; set; }
         public DateTime Time { get; set; }
         public string Type { get; set; }
     }
