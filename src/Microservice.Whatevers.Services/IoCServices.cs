d0d235dae939619b06fba4bb9735086dd6417c20
antoniofalcao <antonio.falcao@softplan.com.br>
2020-02-02 21:06:09 -0300

    Code CleanUp

--- src/Microservice.Whatevers.Services/IoCServices.cs
+++ src/Microservice.Whatevers.Services/IoCServices.cs
@@ -1,8 +1,5 @@
 using AutoMapper;
-using FluentValidation;
 using Microservice.Whatevers.Services.Mapper;
-using Microservice.Whatevers.Services.Models;
-using Microservice.Whatevers.Services.Validators;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microservice.Whatevers.Services
@@ -11,11 +8,7 @@ namespace Microservice.Whatevers.Services
     {
         public static void Register(IServiceCollection services)
         {
-            // services.AddTransient<IValidator<WhateverModel>, WhateverModelValidator>();
-            // services.AddTransient<IValidator<ThingModel>, ThingModelValidator>();
-            
             services.AddAutoMapper(typeof(DomainToModelMappingProfile), typeof(ModelToDomainMappingProfile));
-            
             services.AddScoped<IWhateverService, WhateverService>();
         }
     }
