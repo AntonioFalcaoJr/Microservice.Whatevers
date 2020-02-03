d7fdf0c4482e587331e9607d5aabe2cb05ec9bb0
antoniofalcao <antonio.falcao@softplan.com.br>
2020-02-03 19:05:43 -0300

    Teste com remoção de mapeamento

--- src/Microservice.Whatevers.Services/Mapper/ModelToDomainMappingProfile.cs
+++ src/Microservice.Whatevers.Services/Mapper/ModelToDomainMappingProfile.cs
@@ -1,15 +1,13 @@
-using AutoMapper;
-using Microservice.Whatevers.Domain.Entities;
-using Microservice.Whatevers.Services.Models;
+
 
 namespace Microservice.Whatevers.Services.Mapper
 {
-    public class ModelToDomainMappingProfile : Profile
+    public class ModelToDomainMappingProfile 
     {
         public ModelToDomainMappingProfile()
         {
-            CreateMap<WhateverModel, Whatever>();
-            CreateMap<ThingModel, Thing>();
+            // CreateMap<WhateverModel, Whatever>();
+            // CreateMap<ThingModel, Thing>();
         }
     }
 }
