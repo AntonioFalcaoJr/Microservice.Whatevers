199d287bf66c8fb533108cdc763d050181a37205
antoniofalcao <antonio.falcao@softplan.com.br>
2020-02-04 19:51:32 -0300

    Adicionando WhateverControllerBase.cs

---
+++ src/Microservice.Whatevers.Api/Controllers/WhateverControllerBase.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice.Whatevers.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class WhateverControllerBase : ControllerBase
+    {
+        
+    }
+}
