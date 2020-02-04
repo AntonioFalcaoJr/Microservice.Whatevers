ee1db2443b160207de4b7ad692a2ea91b172013b
antoniofalcao <antonio.falcao@softplan.com.br>
2020-02-04 20:00:16 -0300

    Code CleanUp

--- src/Microservice.Whatevers.Api/Controllers/WhateverControllerBase.cs
+++ src/Microservice.Whatevers.Api/Controllers/WhateverControllerBase.cs
@@ -2,10 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Microservice.Whatevers.Api.Controllers
 {
-    [ApiController]
-    [Route("api/v{version:apiVersion}/[controller]")]
-    public class WhateverControllerBase : ControllerBase
-    {
-        
-    }
+    [ApiController, Route("api/v{version:apiVersion}/[controller]")]
+    public class WhateverControllerBase : ControllerBase { }
 }
