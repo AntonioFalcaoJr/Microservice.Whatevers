bd2ddbda62da7edf1ff3a90ec163f1105787b3a5
antoniofalcao <antonio.falcao@softplan.com.br>
2020-02-06 09:55:09 -0300

    Rename

--- src/Microservice.Whatevers.Services/WhateverService.cs
+++ src/Microservice.Whatevers.Services/WhateverService.cs
@@ -5,7 +5,7 @@ using Microservice.Whatevers.Services.Models;
 
 namespace Microservice.Whatevers.Services
 {
-    public class WhateverService : BaseService<Whatever, WhateverModel>, IWhateverService
+    public class WhateverService : ServiceBase<Whatever, WhateverModel>, IWhateverService
     {
         public WhateverService(IWhateverRepository whateverRepository, IMapper mapper)
             : base(whateverRepository, mapper) { }
