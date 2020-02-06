393718a664dcd30cbcd9d6d282a44f0e322ac255
antoniofalcao <antonio.falcao@softplan.com.br>
2020-02-06 09:54:42 -0300

    rename

--- src/Microservice.Whatevers.Domain/Entities/Whatever.cs
+++ src/Microservice.Whatevers.Domain/Entities/Whatever.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Microservice.Whatevers.Domain.Entities
 {
-    public class Whatever : EntityBase
+    public class Whatever : BaseEntity
     {
         public string Name { get; set; }
         public virtual ICollection<Thing> Things { get; set; }
