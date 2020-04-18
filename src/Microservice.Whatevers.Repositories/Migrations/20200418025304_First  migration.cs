930423bb82284d2f289f0dd6797b2cd7a6b17014
Antônio Falcão <antonio.falcao@softplan.com.br>
2020-04-18 00:19:56 -0300

    Ajuste no relacionamento.

--- src/Microservice.Whatevers.Repositories/Migrations/20200418025304_First  migration.cs
+++ src/Microservice.Whatevers.Repositories/Migrations/20200418025304_First  migration.cs
@@ -45,42 +45,22 @@ namespace Microservice.Whatevers.Repositories.Migrations
             migrationBuilder.InsertData(
                 table: "Whatever",
                 columns: new[] { "Id", "Name", "Time", "Type" },
-                values: new object[] { new Guid("d54c0aab-0014-4270-a6d6-ec3b4f6da372"), "Whatever", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Some type" });
+                values: new object[] { new Guid("059afab1-e03f-4154-8f59-a201ac6b6f72"), "Whatever", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Some type" });
 
             migrationBuilder.InsertData(
                 table: "Whatever",
                 columns: new[] { "Id", "Name", "Time", "Type" },
-                values: new object[] { new Guid("5b72f8d7-4f33-480a-ad05-93672580a456"), "Whatever", new DateTime(9999, 12, 31, 23, 59, 59, 999, DateTimeKind.Unspecified).AddTicks(9999), "Another type" });
+                values: new object[] { new Guid("7480dbeb-913f-4239-bddd-3453dc9ed3bf"), "Whatever", new DateTime(9999, 12, 31, 23, 59, 59, 999, DateTimeKind.Unspecified).AddTicks(9999), "Another type" });
 
             migrationBuilder.InsertData(
                 table: "Whatever",
                 columns: new[] { "Id", "Name", "Time", "Type" },
-                values: new object[] { new Guid("f1934e57-61fa-4a84-b281-d43d005a7331"), "Whatever", new DateTime(2020, 4, 17, 23, 6, 32, 444, DateTimeKind.Local).AddTicks(3156), "More another type" });
+                values: new object[] { new Guid("9aeb431d-7c1f-4c7e-a689-8ce6db155f71"), "Whatever", new DateTime(2020, 4, 17, 23, 53, 3, 808, DateTimeKind.Local).AddTicks(168), "More another type" });
 
             migrationBuilder.InsertData(
                 table: "Whatever",
                 columns: new[] { "Id", "Name", "Time", "Type" },
-                values: new object[] { new Guid("f08e706c-c599-4e9f-8ae1-e873ff404b75"), "Whatever", new DateTime(2020, 4, 17, 0, 0, 0, 0, DateTimeKind.Local), "Once more another type" });
-
-            migrationBuilder.InsertData(
-                table: "Thing",
-                columns: new[] { "Id", "Name", "Type", "Value", "WhateverId" },
-                values: new object[] { new Guid("8cf9506f-a19e-4ece-be56-89ef57572fc7"), "Thing", "Some type", 5.0, new Guid("d54c0aab-0014-4270-a6d6-ec3b4f6da372") });
-
-            migrationBuilder.InsertData(
-                table: "Thing",
-                columns: new[] { "Id", "Name", "Type", "Value", "WhateverId" },
-                values: new object[] { new Guid("4c1c68f7-c95f-4a2a-ba1f-a4ae72c5e530"), "Thing", "Another type", 5.0, new Guid("5b72f8d7-4f33-480a-ad05-93672580a456") });
-
-            migrationBuilder.InsertData(
-                table: "Thing",
-                columns: new[] { "Id", "Name", "Type", "Value", "WhateverId" },
-                values: new object[] { new Guid("865d1e49-165f-4fef-aebd-817133b73828"), "Thing", "More another type", 5.0, new Guid("f1934e57-61fa-4a84-b281-d43d005a7331") });
-
-            migrationBuilder.InsertData(
-                table: "Thing",
-                columns: new[] { "Id", "Name", "Type", "Value", "WhateverId" },
-                values: new object[] { new Guid("3c02b410-b02b-438a-a87e-1226e1a69141"), "Thing", "Once more another type", 5.0, new Guid("f08e706c-c599-4e9f-8ae1-e873ff404b75") });
+                values: new object[] { new Guid("5da530d9-15d3-4ed0-90d8-280658342e44"), "Whatever", new DateTime(2020, 4, 17, 0, 0, 0, 0, DateTimeKind.Local), "Once more another type" });
 
             migrationBuilder.CreateIndex(
                 name: "IX_Thing_WhateverId",
