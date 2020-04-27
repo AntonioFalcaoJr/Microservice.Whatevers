c004f275000266bc9e74eb828784adcfc6c45050
Antônio Falcão <antonio.falcao@softplan.com.br>
2020-04-27 12:21:20 -0300

    Reduzindo o acoplamento

--- src/Microservice.Whatevers.Repositories/IoC/IoCRepositories.cs
+++ src/Microservice.Whatevers.Repositories/IoC/IoCRepositories.cs
@@ -1,16 +1,15 @@
 using Microservice.Whatevers.Repositories.Contexts;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microservice.Whatevers.Repositories.IoC
 {
     public static class IoCRepositories
     {
-        public static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
-            => services.AddDbContext<WhateverContext>(dbContextOptions
+        public static IServiceCollection AddDbContext(this IServiceCollection services, string connectionString) =>
+            services.AddDbContext<WhateverContext>(dbContextOptions
                 => dbContextOptions.UseLazyLoadingProxies()
-                   .UseSqlite(configuration.GetConnectionString("DefaultConnection"), sqliteOptions
+                   .UseSqlite(connectionString, sqliteOptions
                         => sqliteOptions.MigrationsAssembly(typeof(WhateverContext).Assembly.GetName().Name)));
 
         public static IServiceCollection AddRepository(this IServiceCollection services)
