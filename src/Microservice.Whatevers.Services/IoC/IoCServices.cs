5069d5a9e7a2d3bdb378393d8a5badfda03f9dfb
Antônio Falcão <antonio.falcao@softplan.com.br>
2020-05-02 12:33:12 -0300

    Evoluindo o IoC

--- src/Microservice.Whatevers.Services/IoC/IoCServices.cs
+++ src/Microservice.Whatevers.Services/IoC/IoCServices.cs
@@ -1,19 +1,20 @@
 using AutoMapper;
-using Microservice.Whatevers.Services.Clients;
+using FluentValidation.AspNetCore;
 using Microservice.Whatevers.Services.Mapper;
+using Microservice.Whatevers.Services.Validators;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microservice.Whatevers.Services.IoC
 {
-    public static class IocServices
+    public static class IoCServices
     {
         public static IServiceCollection AddAutoMapper(this IServiceCollection services)
-            => services.AddAutoMapper(typeof(ModelToDomainProfile));
+            => services.AddAutoMapper(typeof(ModelToDomainProfile), typeof(DomainToModelProfile));
 
-        public static IServiceCollection AddServices(this IServiceCollection services)
-            => services
-               .AddScoped<IWhateverService, WhateverService>()
-               .AddScoped<IGoogleService, GoogleService>()
-               .AddScoped<IGoogleClient, GoogleClient>();
+        public static IServiceCollection AddServices(this IServiceCollection services) 
+            => services.AddScoped<IWhateverService, WhateverService>();
+
+        public static IMvcCoreBuilder AddFluentValidation(this IMvcCoreBuilder builder) 
+            => builder.AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<WhateverModelValidator>());
     }
 }
